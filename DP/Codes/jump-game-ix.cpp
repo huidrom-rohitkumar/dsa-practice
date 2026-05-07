c32d62eeea48f36039f8626aba960b121409388f
Huidrom Rohitkumar Meitei <rohithuidrom2020@gmail.com>
2026-05-07 07:59:39 +0530

    Add Solution class with maxValue method
    Implement maxValue function to compute maximum values.

---
+++ DP/Codes/jump-game-ix.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> maxValue(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return {};
+
+        vector<int> preMax(n);
+        preMax[0] = nums[0];
+        for (int i = 1; i < n; ++i) {
+            preMax[i] = max(preMax[i - 1], nums[i]);
+        }
+
+        vector<int> ans(n);
+        int sufMin = INT_MAX;
+
+        for (int i = n - 1; i >= 0; --i) {
+            if (preMax[i] > sufMin) {
+                ans[i] = ans[i + 1];
+            } else {
+                ans[i] = preMax[i];
+            }
+            sufMin = min(sufMin, nums[i]);
+        }
+
+        return ans;
+    }
+};
